37929b4efdefa7dc71579162913b6ce1fe4211a4
Najmiddin Nazirov <nnazirov@transoxiania.com>
2026-06-10 15:52:32 +0100

    feat: refactor seriesList to improve query efficiency and structure

--- src/api/series.ts
+++ src/api/series.ts
@@ -1,6 +1,6 @@
 import { type Context } from "hono";
 import { descToJson } from "@/utils/parser";
-import { like, notLike } from "drizzle-orm";
+import { inArray, like, notLike } from "drizzle-orm";
 import { movie } from "@/db/schema";
 import { desc } from "drizzle-orm";
 import { db } from "@/db/client";
@@ -10,8 +10,11 @@ export const seriesList = async (c: Context) => {
     const limit = Math.min(100, Math.max(1, Number(c.req.query("limit") ?? 20)));
     const offset = (page - 1) * limit;
 
-    const movies = await db.select().from(movie).where(like(movie.code, "%N%")).orderBy(desc(movie.created_at)).limit(limit).offset(offset);
+    const seriesall = await db.select().from(movie).where(like(movie.code, "%N%"));
 
-    const data = movies.map((e) => ({ ...e, ...descToJson(e.description), description: undefined }));
+    const ids = [...new Set(seriesall.map((e) => e.code.split("N")[0]))];
+    const series = await db.select().from(movie).where(inArray(movie.code, ids)).orderBy(desc(movie.created_at)).limit(limit).offset(offset);
+
+    const data = series.map((e) => ({ ...e, ...descToJson(e.description), description: undefined }));
     return c.json({ page, limit, data });
 };
