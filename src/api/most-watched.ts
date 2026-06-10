ce786b184df7a0510fdc14080a1dc30c0146a684
Najmiddin Nazirov <nnazirov@transoxiania.com>
2026-06-10 16:22:01 +0100

    feat: enhance mostWatchedMoviesToday query with additional filtering conditions

--- src/api/most-watched.ts
+++ src/api/most-watched.ts
@@ -4,6 +4,10 @@ import { notLike } from "drizzle-orm";
 import { movie } from "@/db/schema";
 import { desc } from "drizzle-orm";
 import { db } from "@/db/client";
+import { and } from "drizzle-orm";
+import { not } from "drizzle-orm";
+import { ne } from "drizzle-orm";
+import { gt } from "drizzle-orm";
 
 export const mostWatchedMovies = async (c: Context) => {
     const page = Math.max(1, Number(c.req.query("page") ?? 1));
@@ -21,7 +25,8 @@ export const mostWatchedMoviesToday = async (c: Context) => {
     const limit = Math.min(100, Math.max(1, Number(c.req.query("limit") ?? 20)));
     const offset = (page - 1) * limit;
 
-    const movies = await db.select().from(movie).where(notLike(movie.code, "%N%")).orderBy(desc(movie.today_count)).limit(limit).offset(offset);
+    const whereCondition = and(notLike(movie.code, "%N%"), gt(movie.today_count, 0));
+    const movies = await db.select().from(movie).where(whereCondition).orderBy(desc(movie.today_count)).limit(limit).offset(offset);
 
     const data = movies.map((e) => ({ ...e, ...descToJson(e.description), description: undefined }));
     return c.json({ page, limit, data });
