8391e982e12c0d6667ef117e6c7c14945f3db2d2
Najmiddin Nazirov <nnazirov@transoxiania.com>
2026-06-10 13:01:43 +0100

    feat: enhance description parsing to include year, genres, duration, rating, and votes

--- src/utils/parser.ts
+++ src/utils/parser.ts
@@ -1,5 +1,32 @@
 export const descToJson = (description: string | null) => {
     const [title, info, plot] = (description || "\n\n\n\n").split("\n\n");
+    const infoArr = info.split("\n");
 
-    return { title, info, plot };
+    const year = infoArr
+        .find((e) => e.includes("Yil"))!
+        .split(":")[1]
+        .trim();
+
+    const genres = infoArr
+        .find((e) => e.includes("Janr"))!
+        .split(":")[1]
+        .trim()
+        .split("#")
+        .map((e) => e.trim());
+
+    const duration = infoArr
+        .find((e) => e.includes("Davomiyligi"))!
+        .split(":")[1]
+        .trim();
+
+    const irating = infoArr
+        .find((e) => e.includes("IMDb reyting"))!
+        .split(":")[1]
+        .trim();
+
+    const match = irating.match(/^([\d.]+)\s*\((\d+)\s+ovoz\)$/);
+    const rating = match![1];
+    const vote = match![2];
+
+    return { title, info, plot, year, rating, vote, genres, duration };
 };
