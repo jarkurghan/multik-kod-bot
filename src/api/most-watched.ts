06083d8d7a6109d081d18024f4e73fcff156252d
Najmiddin Nazirov <nnazirov@transoxiania.com>
2026-06-10 13:25:59 +0100

    feat: enhance mostWatchedMovies to include parsed description data

--- src/api/most-watched.ts
+++ src/api/most-watched.ts
@@ -3,6 +3,7 @@ import { notLike } from "drizzle-orm";
 import { movie } from "@/db/schema";
 import { desc } from "drizzle-orm";
 import { db } from "@/db/client";
+import { descToJson } from "@/utils/parser";
 
 export const mostWatchedMovies = async (c: Context) => {
     const page = Math.max(1, Number(c.req.query("page") ?? 1));
@@ -11,5 +12,6 @@ export const mostWatchedMovies = async (c: Context) => {
 
     const movies = await db.select().from(movie).where(notLike(movie.code, "%N%")).orderBy(desc(movie.total_count)).limit(limit).offset(offset);
 
-    return c.json({ page, limit, data: movies });
+    const data = movies.map((e) => ({ ...e, ...descToJson(e.description), description: undefined }));
+    return c.json({ page, limit, data });
 };
